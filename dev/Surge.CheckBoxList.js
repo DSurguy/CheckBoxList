b524f182c31924f89c2de916a4d570c9d966c1d1
Derek Surguy <Surguy.Derek@gmail.com>
2014-03-13 16:52:48 -0500

    Removing testing frameworks with jsdom dependency
    Removed mocha to make way for build system with qunit and webserver for
    testing

--- dev/Surge.CheckBoxList.js
+++ dev/Surge.CheckBoxList.js
@@ -5,11 +5,20 @@
 
 	$.widget( "Surge.CheckBoxList", {
 		options: {
-			data: []
+			data: [],
+			displayData: [],
+			transform: undefined,
+			header: ""
 		},
 
 		_create: function() {
 			console.log(this.options.data);
+		},
+
+
+
+		select: function(index) {
+			console.log("Selecting Index: "+index);
 		}
 	});
 
