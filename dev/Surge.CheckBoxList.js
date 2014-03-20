3f98945ebcd818290e6a3d9148e258a9259414b8
Derek Surguy <Surguy.Derek@gmail.com>
2014-03-20 10:50:12 -0500

    Implementing creation of widget html and data
    Implemented 2 new test in _create, all tests passing

--- dev/Surge.CheckBoxList.js
+++ dev/Surge.CheckBoxList.js
@@ -6,21 +6,33 @@
 	$.widget( "Surge.CheckBoxList", {
 		options: {
 			data: [],
-			displayData: [],
 			transform: undefined,
 			header: ""
 		},
 
 		_create: function() {
-			//Create the display data if it doesn't exist
-			if( this.options.displayData.length == 0){
-				var tFunc = this.options.transform;
-				if( !tFunc ){
-					tFunc = function(item){ return item.toString(); };
-				}
-				for( var i=0; i<this.options.data.length; i++ ){
-					this.options.displayData[i] = tFunc(this.options.data[i]);
-				}
+			//Create the display data
+			this._displayData = [];
+			var tFunc = this.options.transform;
+			if( !tFunc ){
+				tFunc = function(item){ return item.toString(); };
+			}
+			for( var i=0; i<this.options.data.length; i++ ){
+				this._displayData[i] = tFunc(this.options.data[i]);
+			}
+
+			//create the html for the checkbox list
+			for( var i=0; i<this.options.data.length; i++ ){
+				//create the checklist item
+				var newItem = $("<div class=\"item\"></div>");
+				//set the attribute to the data index
+				newItem.attr("data-index", i);
+				//add the label from the displayData
+				newItem.append("<span class=\"label\">"+this._displayData[i]+"</span>");
+				//add the checkbox
+				newItem.append("<input type=\"checkbox\" />");
+				//add this to the DOM
+				this.element.append(newItem);
 			}
 		},
 
--- test/WidgetSpec.js
+++ test/WidgetSpec.js
@@ -3,20 +3,35 @@ test( "Does qunit work", function(){
 	ok(true);
 });
 
-
 module("_create", {
 	setup: function(){
-		//create a CheckBoxList with 2 data items
-		$("#checkThis").CheckBoxList([{
-			thing: "WORDS",
-			otherThing: 24
-		}, {
-			thing: "WORDS",
-			otherThing: 24
-		}]);	
+
+		//store data on the fixture
+		$("#qunit-fixture")[0]._QDATA = {
+			tFunc: function(item){
+				return item.thing;
+			},
+			data: [{
+				thing: "WORDS",
+				otherThing: 24
+			}, {
+				thing: "WORDS ALSO",
+				otherThing: 24
+			}]
+		};
+
+		var _QDATA = $("#qunit-fixture")[0]._QDATA;
+
+		//create a CheckBoxList with 2 data items and a transform function
+		$("#checkThis").CheckBoxList({
+			data: _QDATA.data,
+			transform: _QDATA.tFunc
+		});	
 	},
 	teardown: function(){
-
+		$("#checkThis").remove();
+		delete $("#qunit-fixture")[0]._QDATA;
+		$("#qunit-fixture").append("<div id=\"checkThis\"></div>");
 	}
 });
 test("It should create a .item for each data item", function(){
@@ -36,4 +51,23 @@ test("An .item should have a span with class '.label'", function(){
 test("An .item should have an attr reference to its data index", function(){
 	var someItem = $("#checkThis .item:first");
 	equal(someItem.attr("data-index"), "0", "Expecting the first .item to point to the 0 index");
+});
+test("An .item (from object) should have a label matching its display data as determined by the transform function", function(){
+	//grab the stored data
+	var _QDATA = $("#qunit-fixture")[0]._QDATA;
+	//grab the auto and manually created labels
+	var itemLabel = $("#checkThis .item:first .label").html();
+	var tFuncLabel = _QDATA.tFunc(_QDATA.data[0]);
+	//compare
+	equal(itemLabel, tFuncLabel, "Expecting the manually transformed data to match the created label");
+});
+test("An .item (from primitive) should have a label matching its display data as determined by the transform function", function(){
+	//gotta create a different CheckBoxList for this one using an array of primitives
+	$("#qunit-fixture").append("<div id=\"checkTwo\"></div>");
+	var data = ["Test", "Test2"];
+	$("#checkTwo").CheckBoxList({data: data});
+
+	var itemLabel = $("#checkTwo .item:first .label").html();
+	var tFuncLabel = data[0];
+	equal(itemLabel, tFuncLabel, "Expecting the primitive data to match the created label");
 });
